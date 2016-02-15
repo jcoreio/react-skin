0a63c1e7f27a9ba3eb84600e8913479c17366318
Andy Edwards <jedwards@fastmail.com>
2016-02-15 16:51:44 -0600

    add defaultComponent option

--- src/lib/createSkinnableComponent.js
+++ src/lib/createSkinnableComponent.js
@@ -5,18 +5,20 @@ import classNames from 'classnames';
  * Creates a Component that renders a skin component injected from React context.
  * @param{string} componentName the componentName for the created Component class.  If componentName is 'Button',
  * then the 'ButtonSkin' component from context will be used to render the Component.
+ * @param{Component} options.defaultComponent the default component to render if no skin component is provided
  * @returns {SkinnableComponent} a Component that renders the skin component `componentName + 'Skin'` from React context.
  */
-export default function createSkinnableComponent(componentName) {
+export default function createSkinnableComponent(componentName, options = {}) {
   const skinName = componentName + 'Skin';
+  const {defaultComponent} = options;
   return class SkinnableComponent extends Component {
     static contextTypes = {
-      [skinName]: PropTypes.any.isRequired,
+      [skinName]: defaultComponent ? PropTypes.any : PropTypes.any.isRequired,
     };
     static displayName = componentName;
     render() {
-      let Element = this.context[skinName];
-      return <Element skinName={skinName} {...this.props}>{this.props.children}</Element>;
+      let Component = this.context[skinName] || defaultComponent;
+      return <Component {...this.props}>{this.props.children}</Component>;
     }
   };
 }
