bcf4ee96ed95fdbdc6a3ea8d3f0abd0f43102098
Andy Edwards <jedwards@fastmail.com>
2016-02-23 08:40:48 -0600

    add ability to use defaultClassName in skinnable components

--- src/lib/createSkinnableComponent.js
+++ src/lib/createSkinnableComponent.js
@@ -4,21 +4,28 @@ import classNames from 'classnames';
 /**
  * Creates a Component that renders a skin component injected from React context.
  * @param{string} componentName the componentName for the created Component class.  If componentName is 'Button',
- * then the 'ButtonSkin' component from context will be used to render the Component.
+ * then the 'ButtonSkin' component from context will be used to render the Component.  Also, if 'ButtonClassName' exists
+ * in the context, it will be appended to the className given to the Component.
  * @param{Component} options.defaultComponent the default component to render if no skin component is provided
+ * @param{string} options.defaultClassName the default className to use if `${componentName}ClassName` is not in
+ * context
+ * @param{string} options.className additional className to always use
  * @returns {SkinnableComponent} a Component that renders the skin component `componentName + 'Skin'` from React context.
  */
 export default function createSkinnableComponent(componentName, options = {}) {
-  const skinName = componentName + 'Skin';
-  const {defaultComponent} = options;
+  const skinKey = componentName + 'Skin';
+  const classNameKey = componentName + 'ClassName';
+  const {defaultComponent, defaultClassName, className: alwaysClassName} = options;
   return class SkinnableComponent extends Component {
     static contextTypes = {
-      [skinName]: defaultComponent ? PropTypes.any : PropTypes.any.isRequired,
+      [skinKey]: defaultComponent ? PropTypes.any : PropTypes.any.isRequired,
+      [classNameKey]: PropTypes.string,
     };
     static displayName = componentName;
     render() {
-      let Component = this.context[skinName] || defaultComponent;
-      return <Component {...this.props}>{this.props.children}</Component>;
+      let Comp = this.context[skinKey] || defaultComponent;
+      let className = classNames(alwaysClassName, this.props.className, this.context[classNameKey] || defaultClassName);
+      return <Comp {...this.props} className={className}>{this.props.children}</Comp>;
     }
   };
 }
